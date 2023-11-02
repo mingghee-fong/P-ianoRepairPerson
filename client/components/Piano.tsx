9886572b2ec043d1f377243ddc73b161fe76ac75
student <student@pop-os.localdomain>
2023-11-03 11:08:36 +1300

    got keys displaying and broken piano image link

--- client/components/Piano.tsx
+++ client/components/Piano.tsx
@@ -1,3 +1,30 @@
 export default function Piano() {
-  return <p>Helloooooooo</p>
+  return (
+    <>
+      <div className="key-grid">
+        <div className="key" id="keyA">
+          Div A
+        </div>
+        <div className="key" id="keyB">
+          Div B
+        </div>
+        <div className="key" id="keyC">
+          Div C
+        </div>
+        <div className="key" id="keyD">
+          Div D
+        </div>
+        <div className="key" id="keyE">
+          Div E
+        </div>
+        <div className="key" id="keyF">
+          Div F
+        </div>
+        <div className="key" id="keyG">
+          Div G
+        </div>
+        <img src="../images/piano.jpg" alt="piano image"></img>
+      </div>
+    </>
+  )
 }
