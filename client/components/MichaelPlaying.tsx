56668299e1c4e2d797ec324642e53bfaede2a2a9
lj-linessmith <142572937+lj-linessmith@users.noreply.github.com>
2023-11-03 11:59:57 +1300

    added breaking function

--- client/components/MichaelPlaying.tsx
+++ client/components/MichaelPlaying.tsx
@@ -1,10 +1,20 @@
-// export default function MichaelPlaying() {
-//   document.addEventListener('keydown', (e) => {
-//     alert('Sorry! You need to pay 99¢ to access this feature')
-//   })
-//   return () => {
-//     document.removeEventListener('keydown', (e) => {
-//       alert('Sorry! You need to pay 99¢ to access this feature')
-//     })
-//   }
-// }
+import React, { useEffect } from 'react';
+
+export default function MichaelPlaying() {
+  // Function to handle keydown event
+  const handleKeydown = (e) => {
+    alert("Sorry! You need to pay 99¢ to access this feature");
+  };
+
+  useEffect(() => {
+    // Add the event listener when the component is mounted
+    document.addEventListener('keydown', handleKeydown);
+
+    // Remove the event listener when the component is unmounted
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, []); // The empty dependency array ensures this effect only runs on mount and unmount
+
+  return <div>Michael's playing component</div>;
+}
